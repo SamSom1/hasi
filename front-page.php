c0bf004817602de13467326110111f1abd25e771
gruncho <alex@codecantor.com>
2016-10-24 11:33:07 +0200

    create static html

--- front-page.php
+++ front-page.php
@@ -1,7 +1,45 @@
-<?php get_header(); ?>  
+<?php get_header(); ?>
 
-	<h1>
-		Use this view as homepage
-	</h1>
+	<div class="home">
+		<h1 class="home__title">
+			The collective idea studio.
+		</h1>
+		<nav class="home__nav">
+			<ul class="home__tags">
+				<li>
+					<a href="#">
+					Applications
+					</a>
+				</li>
+				<li>
+					<a href="#">
+					Arte
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						Software
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<section class="home__list">
+			<article class="home__content home__content--mgn">
+				<div class="home__subtitle">
+					Magna
+				</div>
+			</article>
+			<article class="home__content home__content--lrm">
+				<div class="home__subtitle">
+					Lorem
+				</div>
+			</article>
+			<article class="home__content home__content--fgt">
+				<div class="home__subtitle">
+					Feugiat
+				</div>
+			</article>
+		</section>
+	</div>
 
 <?php get_footer(); ?>
