7b6cde45b2b9822d7387bb415a2c974e05a9db26
gruncho <alex@codecantor.com>
2016-10-25 12:41:02 +0200

    add js

--- footer.php
+++ footer.php
@@ -2,5 +2,10 @@
 	Foot. 
 </footer>
 
+<!-- js scripts -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/app.js"></script>
+
 </body>
 </html>
