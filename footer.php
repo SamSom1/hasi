66f246969bc3d81c73635688228ed3eab1a9ab7f
gruncho <alex@codecantor.com>
2016-10-28 18:28:09 +0200

    add js script

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@ Find us on <a href="mailto:comunidad@watchoutfreedom.com">com</a>, <a href="mail
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/assets/js/app.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/assets/js/jquery.scrollTo.min.js"></script>
 
 </body>
 </html>
