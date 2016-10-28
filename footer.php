19274ecdd104d54be4de4f06685c485d8c168db9
gruncho <alex@codecantor.com>
2016-10-28 18:32:24 +0200

    add example HTML

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<div id="front_page">
+	Ven hasta mi!
+</div>
+
 <footer class="foot">
 Find us on <a href="mailto:comunidad@watchoutfreedom.com">com</a>, <a href="mailto:comunidad@watchoutfreedom.com">Instagram</a> and <a href="mailto:comunidad@watchoutfreedom.com">Twitter</a> </footer>
 
